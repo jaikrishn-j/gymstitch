c123796a13cb756054ed2da33f71ab1a342df060
jaikrishn <jaikrishn2003@gmail.com>
2026-09-17 22:50:19 +0530

    fix vercel issues

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -14,7 +14,7 @@ function Progress({
       value={value}
       data-slot="progress"
       className={cn("flex flex-wrap gap-3", className)}
-      getAriaValueText={(formattedValue, value) => `${Math.round(value)}%`}
+      getAriaValueText={(formattedValue, value) => `${Math.round(value ?? 0)}%`}
       {...props}
     >
       {children}
